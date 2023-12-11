91c7c1ffcbb92e638a99d0e026a06d4ffd554de8
Moosa-Razaa <moosaraza1k99@gmail.com>
2023-12-11 14:55:06 +0100

    Create Event Completed

--- src/pages/event/create/index.tsx
+++ src/pages/event/create/index.tsx
@@ -1,4 +1,5 @@
-import { Button, Form, Input, Modal } from "antd";
+import { Button, DatePicker, Form, Input, Modal } from "antd";
+import style from "./createEvent.module.css";
 
 const modalTitle = "Create New Event";
 const modalOkText = "Create Event";
@@ -13,21 +14,23 @@ enum FromFieldNames
 {
     EVENTNAME = "EventName",
     DESCRIPTION = "Description",
-    DATE = "Date",
-    
+    SCHEDULEDDATE = "SCHEDULEDDATE"
 }
+
+const {TextArea} = Input;
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
 
 function CreateEvent({openModal, setOpenModal} : Props)
 {
     const [form] = Form.useForm();
 
-    function OnFormFinish()
-    {
-        console.log("Name : ", form.getFieldValue("name"));
-    }
-
     function ModalOnCancelHandler()
     {
+        form.resetFields();
         setOpenModal(false);
     }
 
@@ -38,13 +41,28 @@ function CreateEvent({openModal, setOpenModal} : Props)
             open={openModal}
             onCancel={ModalOnCancelHandler}
             okText={modalOkText}
+            width={750}
+            footer={null}
         >
-            <Form form={form} onFinish={OnFormFinish}>
-                <Form.Item name="name" label="Name" required={true} preserve={false}>
+            <Form {...layout} form={form} style={{ width: 600 }}>
+                <Form.Item name={FromFieldNames.EVENTNAME} label="Name" required={true}>
                     <Input />
                 </Form.Item>
 
-                <Form.Item label=" ">
+                <Form.Item name={FromFieldNames.SCHEDULEDDATE} label="Scheduled Date" required={true}>
+                    <DatePicker className={style["datePicker"]} />
+                </Form.Item>
+
+                <Form.Item name={FromFieldNames.DESCRIPTION} label="Description">
+                    <TextArea
+                        showCount={true}
+                        maxLength={100}
+                        placeholder="description"
+                        style={{ resize: "none", height: 120 }}
+                    />                    
+                </Form.Item>
+
+                <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
                     <Button type="primary" htmlType="submit">Submit</Button>
                 </Form.Item>
             </Form>
