df699bcce7b043e1807e946d1f810091076a9431
Moosa-Razaa <moosaraza1k99@gmail.com>
2023-12-12 13:21:47 +0100

    Add basic structure to calendar

--- src/pages/calender/index.tsx
+++ src/pages/calender/index.tsx
@@ -1,10 +1,49 @@
-import Event from "../event";
+import { format } from "date-fns";
+import { Col, Row, Typography } from 'antd';
+import style from "./style.module.css";
+
+const { Title } = Typography;
+
+const gridRowStyles: React.CSSProperties = {
+    height: "150px",
+    marginTop: "50px"
+}
+
+const gridDaysOfWeekStyles: React.CSSProperties = {
+    height: "100%",
+    width: "100%",
+    textAlign: "center",
+    verticalAlign: "middle"
+}
+
+const daysOfWeek: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 export default function Calender()
 {
+    const currentDate = new Date();
+    const monthAndYear = format(currentDate, "MMMM yyyy");
+
     return(
-        <div>
-            <Event />
+        <div className={style["mainCalenderDiv"]}>
+            <header className={style["mainMonthAndYearHeading"]}>
+                <div className={style["mainHeaderDiv"]}>
+                    <Title style={{ color: "#D3D6DB" }}>{monthAndYear}</Title>
+                </div>
+            </header>
+
+            <main>
+                <Row style={gridRowStyles} align="middle" justify="space-between" gutter={[4, 4]}>
+                    {
+                        daysOfWeek.map((currentDay) => { 
+                            return <Col span={3} key={currentDay} style={gridDaysOfWeekStyles}>
+                                <div className={style["daysDiv"]}>
+                                    <Title style={{ color: "#3A4750"}} level={3}>{currentDay}</Title>
+                                </div>
+                            </Col>
+                        })
+                    }
+                </Row>
+            </main>
         </div>
     );
 }
