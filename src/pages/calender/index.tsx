247a1d4b5564ce5b06ce6acb769eb3571e70c4ec
Moosa-Razaa <moosaraza1k99@gmail.com>
2023-12-13 14:23:04 +0100

    Improvement: Found better way of rendering the days of week

--- src/pages/calender/index.tsx
+++ src/pages/calender/index.tsx
@@ -1,20 +1,8 @@
 import { format } from "date-fns";
-import { Col, Row, Typography } from 'antd';
+import { Typography } from 'antd';
 import style from "./style.module.css";
 
 const { Title } = Typography;
-
-const gridRowStyles: React.CSSProperties = {
-    height: "150px",
-    marginTop: "50px"
-}
-
-const gridDaysOfWeekStyles: React.CSSProperties = {
-    height: "100%",
-    width: "100%",
-    textAlign: "center",
-    verticalAlign: "middle"
-}
 
 const daysOfWeek: string[] = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
@@ -31,18 +19,12 @@ export default function Calender()
                 </div>
             </header>
 
-            <main>
-                <Row style={gridRowStyles} align="middle" justify="space-between" gutter={[4, 4]}>
+            <main>       
+                <div className={style["weekdaysGrid"]}>
                     {
-                        daysOfWeek.map((currentDay) => { 
-                            return <Col span={3} key={currentDay} style={gridDaysOfWeekStyles}>
-                                <div className={style["daysDiv"]}>
-                                    <Title style={{ color: "#3A4750"}} level={3}>{currentDay}</Title>
-                                </div>
-                            </Col>
-                        })
+                        daysOfWeek.map((currentDay) => <div className={style["inidividualDay"]}>{currentDay}</div>)
                     }
-                </Row>
+                </div>
             </main>
         </div>
     );
